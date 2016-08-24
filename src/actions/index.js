9471f5731f00ebfc978696d064a1146e8470a66e
Kenzo Mendoza <kenzom954@gmail.com>
2016-08-24 12:38:30 -0700

    add actions redux

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export const RECENT_DATA = 'RECENT_DATA';
+export const ALLTIME_DATA = 'ALLTIME_DATA';
+
+export function recentData(){
+  const request = axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+  return {
+    type: RECENT_DATA,
+    payload: request
+  };
+}
+export function allTimeData(){
+  const request = axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+  return {
+    type: ALLTIME_DATA,
+    payload: request
+  };
+}
