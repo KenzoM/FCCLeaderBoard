75deb3104f76fae1fd30241cafb1e79e49830995
Kenzo Mendoza <kenzom954@gmail.com>
2016-08-23 20:16:49 -0700

    add simple table component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import LeaderBoard from '../containers/table_board'
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <LeaderBoard />
+      </div>
     );
   }
 }
