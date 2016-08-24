22be17e7fbc1baac359af5db06150ba4ac81e881
Kenzo Mendoza <kenzom954@gmail.com>
2016-08-24 12:43:07 -0700

    need to fix bug on ranking #

--- src/containers/table_board.js
+++ src/containers/table_board.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
+import { recentData } from '../actions/index'
+import { allTimeData } from '../actions/index'
 
 export default class TableBoard extends Component {
+  constructor(props){
+    super(props);
+  }
+  componentDidMount() {
+    this.props.recentData() //fetch data most recent top
+  }
+  renderData(userData){
+    const name = userData.username;
+    const recent = userData.recent;
+    const allTime = userData.alltime;
+    let rank = 1;
+    return(
+      <tr key={name}>
+        <td>{rank}</td>
+        <td>{name}</td>
+        <td>{recent}</td>
+        <td>{allTime}</td>
+      </tr>
+    )
+  }
+  getRecentData(){
+    console.log('recent data')
+    this.props.recentData()
+  }
+  getAllTimeData(){
+    console.log('all-time data')
+    this.props.allTimeData();
+  }
   render(){
     return(
       <table className="table table-hover">
@@ -10,19 +40,30 @@ export default class TableBoard extends Component {
           <tr>
             <th>#</th>
             <th>Camper Name</th>
-            <th>Points in 30 days</th>
-            <th>All-time Posts</th>
+            <th
+              onClick={this.getRecentData.bind(this)}
+              >Points in 30 days
+            </th>
+            <th
+              onClick={this.getAllTimeData.bind(this)}
+              >All-time Posts
+            </th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-             <td></td>
-             <td></td>
-             <td></td>
-             <td></td>
-          </tr>
+          {this.props.FCCData.map(this.renderData)}
         </tbody>
       </table>
     )
   }
 }
+function mapStateToProps(state){
+  return {
+    FCCData: state.collectData
+  }
+}
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ recentData, allTimeData }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TableBoard);
