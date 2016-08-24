9fa9463763ca5de5731b49c62409b6a873101acf
Kenzo Mendoza <kenzom954@gmail.com>
2016-08-24 12:35:52 -0700

    add header component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class Header extends Component{
+  render(){
+    return (
+      <div className="page-header">
+        <h1>FCC LeaderBoard</h1>
+      </div>
+    )
+  }
+}
