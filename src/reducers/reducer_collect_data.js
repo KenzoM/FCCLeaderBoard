3ea562f25b0dace3c20de229973eb8534577dbe5
Kenzo Mendoza <kenzom954@gmail.com>
2016-08-24 13:50:12 -0700

    removed console logs

--- src/reducers/reducer_collect_data.js
+++ src/reducers/reducer_collect_data.js
@@ -3,10 +3,8 @@ import {ALLTIME_DATA} from '../actions/index';
 export default function(state = [], action) {
   switch (action.type){
     case RECENT_DATA:
-      console.log(action.payload.data, 'this is recent data')
       return action.payload.data
     case ALLTIME_DATA:
-      console.log(action.payload.data, 'this is alltime data baby')
       return action.payload.data
   }
   return state
