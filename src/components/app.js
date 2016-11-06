5caa9f16a935e5a3a1dd4bfefb9d9bdedb303756
Kenzo Mendoza <kenzom954@gmail.com>
2016-11-05 20:34:46 -0700

    updated scss

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import TableBoard from '../containers/table_board';
 import Header from './header';
 
-import style from '../stylesheets/style.css';
+import style from '../stylesheets/style.scss';
 
 export default class App extends Component {
   render() {
